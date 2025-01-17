10850b84868867f550049ce6ca6d49a6301de581
DAW <juanmiguel.valle@iesdonana.org>
2025-01-17 13:44:52 +0100

    anyadido profesor 9

--- U05_A08_Valle_Fernández_Juan Miguel/script.js
+++ U05_A08_Valle_Fernández_Juan Miguel/script.js
@@ -1,16 +1,51 @@
-document.getElementById('formulario').addEventListener('submit', function(){
+document.getElementById('formulario').addEventListener('submit', function (event) {
+    event.preventDefault();
+    //coje el nombre
     var nombre = document.getElementById('nombre').value;
+    //coje el mensaje
     var mensaje = document.getElementById('mensaje').value;
+    //coje el color
+    const opciones = document.querySelectorAll('input[name="color"]');
+    //recorre todas las opciones existentes y guarda en la varibale la que tenga la propiedad cheked
+    var color = "";
+    for (let i = 0; i < opciones.length; i++) {
+        if (opciones[i].checked) {
+            color = opciones[i].value;
+        }
+    }
+    //asignatura
+    var lista = document.getElementById('lista').value;
 
-    const opciones = document.getElementById('color');
-
-    for (let i = 0; i < opciones.clientHeight; i++){
-        if(opciones.cheked){
-            var color = opciones[i];
+    //dias disponibles
+    var checkbox = document.querySelectorAll('input[type="checkbox"]');
+    var seleccionado = [];
+    for (let i = 0; i < checkbox.length; i++) {
+        if (checkbox[i].checked) {
+            seleccionado.push(checkbox[i].value);
         }
     }
 
-    var lista = document.getElementById('lista').value;
-    var checkbox =document.getElementById('')
+    //dia de la semana
+    var preferente = document.getElementById('preferente').value;
+
+    //profesor
+    var profesor = "";
+
+    switch (lista) {
+        case "lengua":
+            profesor = "Paco";
+            break;
+        case "matematicas":
+            profesor = "Manuel";
+            break;
+        case "naturales":
+            profesor = "Grabiel";
+            break;
+        case "musica":
+            profesor = "Alfonso";
+            break;
+    }
+
+    window.open(`resultado.html?nombre=${nombre}&mensaje=${mensaje}&color=${color}&asignatura=${lista}&seleccionado=${seleccionado}&preferente=${preferente}&profesor=${profesor}`);
 
 });
