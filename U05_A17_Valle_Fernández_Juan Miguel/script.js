7496bcf8e472c84403b19bfe364104b435181ea7
DAW <juanmiguel.valle@iesdonana.org>
2025-01-21 18:52:37 +0100

    termianada act 17

--- U05_A17_Valle_Fernández_Juan Miguel/script.js
+++ U05_A17_Valle_Fernández_Juan Miguel/script.js
@@ -7,20 +7,46 @@ window.addEventListener('load', function(){
     }, 3000);
 });
 
-var selector = document.getElementById('formulario');
+var selector = document.querySelector('#formulario');
 var nombre = "";
 var color = "";
 var notificaciones = "";
 var selectorNotificaciones = document.querySelectorAll('input[name="notificaciones"]');
-selector.addEventListener('submit', function(){
-    nombre = document.getElementById('nombre').value;
+
+var test = false;
+selector.addEventListener('input', function(){
+
+
+    nombre = document.getElementById('nombre').value.trim();
+    const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+    test = regex.test(nombre);
+    if(test == false || nombre === ""){
+        document.getElementById('errNombre').innerHTML = 'Formato no valido o campo vacio.';
+    } else {
+        document.getElementById('errNombre').innerHTML = '';
+
+    };
 
     color = document.getElementById('color').value;
 
     selectorNotificaciones.forEach(element => {
-        if(element[i].checked){
-            notificaciones = element[i].value;
+        if(!element.checked){
+            document.getElementById('errNotificaciones').innerHTML = 'Falta seleccionar una de las dos opciones.';
+        }else if(element.checked){
+            notificaciones = element.value;
+            document.getElementById('errNotificaciones').innerHTML = '';
         };
     });
+
+    document.getElementById('reNombre').innerHTML = nombre;
+    document.getElementById('reColor').innerHTML = color;
+    document.getElementById('reNotificaciones').innerHTML = notificaciones;
+});
+
+document.getElementById('boton').addEventListener('mouseover',function(){
+    document.getElementById('boton').style.backgroundColor = 'red';
+})
+document.getElementById('boton').addEventListener('mouseout', function () {
+    document.getElementById('boton').style.backgroundColor = 'blue'; // Vuelve al color original
 });
 
