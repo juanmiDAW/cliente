18e4864c7ac5957f4853d1a61211926bc2d7dc4d
daw <juanmiguel.valle@iesdonana.org>
2025-02-06 17:13:53 +0100

    correccion de mi amigo ale

--- U06_A02_Valle_Fernández_Juan Miguel/script.js
+++ U06_A02_Valle_Fernández_Juan Miguel/script.js
@@ -1,11 +1,18 @@
+//obtencion de los elementos
 var anyadir = document.getElementById('anyadir');
 var eliminar = document.getElementById('eliminar');
+
+//creacion de prompt para introducir el texto
 var texto = prompt('Escribe tu texto aqui.');
 
+//se le añade un evento de click
 anyadir.addEventListener('click', function(){
-    var conetenedor = document.createElement('div');
-    conetenedor.innerHTML = texto;
-    document.body.appendChild(conetenedor);
+    //creacion del elemento div
+    var contenedor = document.createElement('div');
+    //se muestra el contenido en el contenedor 
+    contenedor.innerHTML = texto;
+    //se añade el contenedor como ultimo hijo del body
+    document.body.appendChild(contenedor);
 });
 
 eliminar.addEventListener('click', function(){
