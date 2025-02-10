dc78735df5163c945940a168efb44434e34baf8c
DAW <juanmiguel.valle@iesdonana.org>
2025-02-10 11:05:29 +0100

    6-12 terminada

--- U06_A12_Valle_Fernández_Juan Miguel/script.js
+++ U06_A12_Valle_Fernández_Juan Miguel/script.js
@@ -1,13 +1,10 @@
+//obtenemos el elemento con id anyadir 
 var selectorAñadir = document.getElementById("anyadir");
 
-
-
-
-// Evento para añadir tarea
+// evento click para añadir tarea
 selectorAñadir.addEventListener("click", function () {
 
-    // Crear el elemento li para la tarea
-
+    // crear el elemento li para la tarea
     var tabla = document.getElementById("tabla");
 
     //si no existe la tabla la creamos
@@ -35,42 +32,125 @@ selectorAñadir.addEventListener("click", function () {
         var celda4 = fila.insertCell();
         celda4.textContent = "Opciones";
 
-
-
-
-
         //creamos el cuerpo de la tabla
         var tbody = document.createElement("tbody");
+
+        //le damos un id al cuerpo de la tabla
         tbody.id = "tabla-body";
+
+        //añadimos el cuerpo a la tabla como ultimo hijo
         tabla.appendChild(tbody);
 
-        //  agregamos la tabla al body
+        //  agregamos la tabla al body del HTML
         document.body.appendChild(tabla);
     }
 
+    //obtenemos el cuerpo de la tabla
     var cuerpo = document.getElementById("tabla-body");
+    //creamos una fila en el cuerpo
     var filaBody = cuerpo.insertRow();
+    //creamos las celdas de la fila
     var celdaBody = filaBody.insertCell();
+    //añadimos el texto a la celda
     celdaBody.textContent = 'predeterminado';
+
+    //evento para editar la celda haciendo doble clic
     celdaBody.addEventListener("dblclick", function (event) {
-        if(event.target.tagName === "TD" && event.target.firstChild){
-        var nuevoTexto = input.value;
-        celdaBody.replaceChild(document.createTextNode(nuevoTexto), input);
-        }
+        //si el elemento clickeado es una celda y tiene un hijo
+        if (event.target.tagName === "TD" && event.target.firstChild) {
+            //obtenemos la celda sobre la que se hizo clic
+            var celda = event.target;
+            //obtenemos el texto de la celda
+            var textoCelda = celda.firstChild.textContent;
+            //creamos un input
+            var input = document.createElement("input");
+            //le damos un id al input
+            input.id = "input";
+            //le asignamos el valor del texto de la celda al input
+            input.value = textoCelda;
+            //reemplazamos el texto por el input
+            celda.replaceChild(input, celda.firstChild);
+            //obtenemos el inputq
+            var selectorInput = document.getElementById("input");
+            //evento para guardar el cambio cuando se presiona enter
+            selectorInput.addEventListener("blur", function (event) {
+                //si el input no tiene el foco
+                if (document.activeElement !== input) {
+                    //obtenemos el nuevo texto del input
+                    var nuevoTexto = input.value;
+                    //reemplazamos el input por el texto con el valor modificado
+                    celda.replaceChild(document.createTextNode(nuevoTexto), input);
+                };
+
+                //evento para guardar el cambio cuando se presiona enter
+                input.addEventListener("keyup", function (event) {
+                    //si se presiona enter
+                    if (event.key === "Enter") {
+                        //obtenemos el nuevo texto del input
+                        var nuevoTexto = input.value;
+                        //reemplazamos el input por el texto con el valor modificado
+                        celda.replaceChild(document.createTextNode(nuevoTexto), input);
+                    };
+
+                });
+            });
+        };
     });
 
     var celdaBody1 = filaBody.insertCell();
     celdaBody1.textContent = 'predeterminado';
+
     celdaBody1.addEventListener("dblclick", function (event) {
-        var nuevoTexto = input.value;
-        celdaBody1.replaceChild(document.createTextNode(nuevoTexto), input);
+        if (event.target.tagName === "TD" && event.target.firstChild) {
+            var celda = event.target;
+            var textoCelda = celda.firstChild.textContent;
+            var input = document.createElement("input");
+            input.id = "input1";
+            input.value = textoCelda;
+            celda.replaceChild(input, celda.firstChild);
+            var selectorInput = document.getElementById("input1");
+            selectorInput.addEventListener("blur", function (event) {
+                if (document.activeElement !== input) {
+                    var nuevoTexto = input.value;
+                    celda.replaceChild(document.createTextNode(nuevoTexto), input);
+                };
+                input.addEventListener("keyup", function (event) {
+                    if (event.key === "Enter") {
+                        var nuevoTexto = input.value;
+                        celda.replaceChild(document.createTextNode(nuevoTexto), input);
+                    };
+
+                });
+            });
+        };
     });
 
     var celdaBody2 = filaBody.insertCell();
     celdaBody2.textContent = 'predeterminado';
+
     celdaBody2.addEventListener("dblclick", function (event) {
-        var nuevoTexto = input.value;
-        celdaBody2.replaceChild(document.createTextNode(nuevoTexto), input);
+        if (event.target.tagName === "TD" && event.target.firstChild) {
+            var celda = event.target;
+            var textoCelda = celda.firstChild.textContent;
+            var input = document.createElement("input");
+            input.id = "input2";
+            input.value = textoCelda;
+            celda.replaceChild(input, celda.firstChild);
+            var selectorInput = document.getElementById("input2");
+            selectorInput.addEventListener("blur", function (event) {
+                if (document.activeElement !== input) {
+                    var nuevoTexto = input.value;
+                    celda.replaceChild(document.createTextNode(nuevoTexto), input);
+                };
+                input.addEventListener("keyup", function (event) {
+                    if (event.key === "Enter") {
+                        var nuevoTexto = input.value;
+                        celda.replaceChild(document.createTextNode(nuevoTexto), input);
+                    };
+
+                });
+            });
+        };
     });
 
     var celdaBody3 = filaBody.insertCell();
@@ -82,29 +162,6 @@ selectorAñadir.addEventListener("click", function () {
     });
 
 
-    
+
 
 });
-
-// Evento para editar tarea (doble clic)
-// listaTareas.addEventListener("dblclick", function (e) {
-
-//     // Si el elemento clickeado es un LI y tiene un hijo
-//     if (e.target.tagName === "LI" && e.target.firstChild) {
-//         var li = e.target;
-//         var textoTarea = li.firstChild.textContent;
-//         var input = document.createElement("input");
-//         input.value = textoTarea;
-
-//         // Reemplazar el texto con el input
-//         li.replaceChild(input, li.firstChild);
-
-//         // Evento para guardar el cambio cuando presionen Enter
-//         input.addEventListener("keyup", function (event) {
-//             if (event.key === "Enter") {
-//                 var nuevoTexto = input.value;
-//                 li.replaceChild(document.createTextNode(nuevoTexto), input);
-//             }
-//         });
-//     }
-// });
