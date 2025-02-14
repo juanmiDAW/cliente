49ed9db4bd54ede28d233a2d40996e9f5e93c4f5
DAW <juanmiguel.valle@iesdonana.org>
2025-02-14 12:13:59 +0100

    documentacion

--- U07_A02_Valle_Fernández_Juan Miguel/ciudadessugerencia.php
+++ U07_A02_Valle_Fernández_Juan Miguel/ciudadessugerencia.php
@@ -1,19 +1,29 @@
 <?php
+//creacion del array que contiene las ciudades
 $ciudades = ['Huelva', 'Sevilla', 'Córdoba', 'Jaen', 'Cádiz', 'Malaga', 'Granada', 'Almeria'];
-
+//se obtiene los datos introducidos de la peticion get
 $ciudad = $_GET['ciudad'];
 
-if ($ciudad !== ''){
+//si lo que se introudce no esta vacio..
+if ($ciudad !== '') {
+    //se crea el array de sugrencias
     $sugerencias = [];
+    //se convierte lo introducido a minusculas 
     $ciudad = strtolower($ciudad);
+    //se obtiene la longitud de la cadena con el nombre de ciudad
     $longitud = strlen($ciudad);
 
-    foreach ($ciudades as $sugerecia){
-        if (stristr($sugerecia, substr($ciudad, 0, $longitud))){
+    //se recorre el array de ciudades
+    foreach ($ciudades as $sugerecia) {
+        //si la ciudad introducida coincide con alguna de las ciudades del array;;
+        if (stristr($sugerecia, substr($ciudad, 0, $longitud))) {
+            //se añade al array de sugerencias
             $sugerencias[] = $sugerecia;
         }
     }
+    //se convierte el array de sugerencias a formato json
     echo json_encode($sugerencias);
-}else{
+} else {
+    //si no se introduce nada se devuelve un array vacio
     echo json_encode('');
 }
--- U07_A02_Valle_Fernández_Juan Miguel/script.js
+++ U07_A02_Valle_Fernández_Juan Miguel/script.js
@@ -1,17 +1,24 @@
+//obtenemos el elelemnto donde se introducira los datos
 var selectorInput = document.getElementById('ciudad');
 
+//añadimos el evento para se actualiza al cambiar de estado el input
 selectorInput.addEventListener('input', async function(){
 
+    //creacion de la variable donde se alamacenara lo que metemos de dato
     var ciudad = selectorInput.value;
 
+    //creacion de la url donde metemos el valor de los datos obtenidos
     const requestURL = 'ciudadessugerencia.php?ciudad=' + ciudad;
-
+    //creacion de la peticion con el obtjeto Request
     const request = new Request(requestURL);
+    //se hace la peticion y hay que esperar a obtener la respuesta y se alamcena la respuesta en la variable response
     const response = await fetch(request);
-    console.log(response);
+    //se convierte la respuesta a texto y se alamacena en la variable
     const ciudadText = await response.text();
+    //se convierte el de JSON a objeto y se almacena en la variable
     const ciudadResultado = JSON.parse(ciudadText);
 
+    //se muestra el resultado en el elemento con id sugerencias
     document.getElementById('sugerencias').innerHTML = ciudadResultado;
 
     
