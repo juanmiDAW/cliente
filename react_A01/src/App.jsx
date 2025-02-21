4d1c077c8905a5422f98846b1f4fb0e0efbbb9ee
daw <juanmiguel.valle@iesdonana.org>
2025-02-21 17:10:53 +0100

    la 1 documentada

--- react_A01/src/App.jsx
+++ react_A01/src/App.jsx
@@ -4,16 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  //se declara la variable count y la funcion se llamará setCount que se iniciará a 0
   const [count, setCount] = useState(0)
 
+  //devuelve un elemento padre que es el div
   return (
-    <>
       <div className="card">
+        {/*boton que al ser pulsado se llama a la funcion que modifica el estado de la variable count*/ }
         <button onClick={() => setCount((count) => count + 1)}>
           {count}
         </button>
       </div>
-    </>
   )
 }
 
